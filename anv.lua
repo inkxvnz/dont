8e44ea908d4a5ac0d853fe6918bf77c6b8a9042a
inkbitnaja <phurinat.sorn@gmail.com>
2025-08-13 23:59:29 +0700

    up fps

--- anv.lua
+++ anv.lua
@@ -1,3 +1,3 @@
-setfpscap(8)
+setfpscap(10)
 script_key="vgCyfcOgMxbfTCLgPqaitckkSxwaqFsx";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c738d23907a049cf3ae3a34ce796464c.lua"))()
